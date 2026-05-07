e9ade3880fadaaf499cfcc35bc2bbc07bef25c7a
Reiner Huechting <reinerh-dev@pm.me>
2026-05-07 23:26:46 +0200

    Remove solution markers.

--- game/game.go
+++ game/game.go
@@ -11,16 +11,12 @@ func New(title string, genre string) *Game {
 	// Hinweis: Erstellen Sie ein neues Game-Objekt mit dem übergebenen Titel und Genre.
 	//          Liefern Sie einen Pointer auf das neue Game-Objekt zurück.
 
-	// begin:solution
 	return &Game{Title: title, Genre: genre}
-	// end:solution
 }
 
 // HasGenre prüft, ob das Spiel ein bestimmtes Genre hat.
 func (g *Game) HasGenre(genre string) bool {
 	// Hinweis: Vergleichen Sie den Genre-String des Spiels mit dem übergebenen Genre-String.
 
-	// begin:solution
 	return g.Genre == genre
-	// end:solution
 }
--- player/player.go
+++ player/player.go
@@ -13,28 +13,22 @@ func New(name string) *Player {
 	// Hinweis: Erstellen Sie ein neues Player-Objekt mit dem übergebenen Namen und einem leeren Map für gespielte Spiele.
 	//          Liefern Sie einen Pointer auf das neue Player-Objekt zurück.
 
-	// begin:solution
 	return &Player{Name: name, playedGames: map[string]int{}}
-	// end:solution
 }
 
 // PlayGame fügt ein Spiel zu den gespielten Spielen des Spielers hinzu und erhöht die Anzahl der gespielten Stunden.
 func (p *Player) PlayGame(g *game.Game, hours int) {
 	// Hinweis: Fügen Sie das Spiel mit der Anzahl der gespielten Stunden zur Map `playedGames` des Spielers hinzu.
 
-	// begin:solution
 	p.playedGames[g.Title] = hours
-	// end:solution
 }
 
 // HasPlayed prüft, ob der Spieler ein bestimmtes Spiel gespielt hat.
 func (p *Player) HasPlayed(g *game.Game) bool {
 	// Hinweis: Überprüfen Sie, ob das Spiel in der Map `playedGames` des Spielers vorhanden ist.
 
-	// begin:solution
 	_, played := p.playedGames[g.Title]
 	return played
-	// end:solution
 }
 
 // HasPlayedMore prüft, ob der Spieler ein bestimmtes Spiel mindestens `hours` Stunden gespielt hat.
@@ -42,10 +36,8 @@ func (p *Player) HasPlayedMore(g *game.Game, hours int) bool {
 	// Hinweis: Überprüfen Sie, ob die gespielten Stunden für das Spiel in der Map `playedGames`
 	// des Spielers größer oder gleich `hours` sind.
 
-	// begin:solution
 	playedHours := p.playedGames[g.Title]
 	return playedHours >= hours
-	// end:solution
 }
 
 // PlayedGames liefert eine Liste mit den Spielen, die der
@@ -54,7 +46,6 @@ func (p *Player) PlayedGames(hours int) []string {
 	// Hinweis: Durchsuchen Sie die Map `playedGames` des Spielers und sammeln Sie die Titel der Spiele,
 	// die der Spieler mindestens `hours` Stunden gespielt hat, in einer Liste.
 
-	// begin:solution
 	games := []string{}
 	for title, playedHours := range p.playedGames {
 		if playedHours >= hours {
@@ -62,5 +53,4 @@ func (p *Player) PlayedGames(hours int) []string {
 		}
 	}
 	return games
-	// end:solution
 }
